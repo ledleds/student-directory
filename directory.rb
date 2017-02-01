a41c7e8b49d740d324d22b5b74f673a5d8b12ea7
Vicky Ledsom <victorialedsom@gmail.com>
2017-02-01 15:00:11 +0000

    Second refactor interactive menu

--- directory.rb
+++ directory.rb
@@ -1,5 +1,37 @@
 @students = [] # an empty array accesible to all methods
 
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit #This causes the programme to terminate
+  else
+    puts "I don't know what you meant, try again."
+  end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
 def input_students
     puts "Please enter the names of the students."
     puts "To finish, just hit return twice."
@@ -139,39 +171,4 @@ def print_footer
   end
 end
 
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit"
-end
-
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
-
-def process(selection)
-  case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when "9"
-    exit #This causes the programme to terminate
-  else
-    puts "I don't know what you meant, try again."
-  end
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end
-
 interactive_menu
-print_header
-print_students_list
-print_footer
