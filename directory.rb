dbfb1af70b62c21d4ea81b094102a51c9d69f304
Vicky Ledsom <victorialedsom@gmail.com>
2017-02-01 14:49:43 +0000

    Refactor interactive menu

--- directory.rb
+++ directory.rb
@@ -1,14 +1,14 @@
+@students = [] # an empty array accesible to all methods
+
 def input_students
     puts "Please enter the names of the students."
     puts "To finish, just hit return twice."
-    # create an empty array
-    students = []
     # get the first name
     name = gets.strip
     # while name is not empty, repeat this code
     while !name.empty? do
-      students << {name: name}
-      count = students.count
+      @students << {name: name}
+      count = @students.count
       if count == 1
         puts "We now have #{count} student."
       else count > 1
@@ -17,12 +17,10 @@ def input_students
       # get another name from the user
       name = gets.strip
     end
-    # return array of students
-    students
 end
 
-def input_cohort(students)
-  students.each do |student|
+def input_cohort
+  @students.each do |student|
     puts "Please enter the cohort for #{student[:name]}"
     cohort = gets.strip.capitalize
 
@@ -44,15 +42,15 @@ def input_cohort(students)
   end
 end
 
-def jan_cohort(students)
+def jan_cohort
   puts "These are the students in the January cohort:"
-  students.select {|student| student[:cohort] == "Jan" || student[:cohort] == "January" }.each_with_index do |student, index|
+  @students.select {|student| student[:cohort] == "Jan" || student[:cohort] == "January" }.each_with_index do |student, index|
     puts "#{index +1}. #{student[:name]} (#{student[:cohort]} cohort)".center(29)
   end
 end
 
-def student_information(students)
-  students.each do |student|
+def student_information
+  @students.each do |student|
     # Enter country information
     puts "Add additional information regarding your student: #{student[:name]}"
     puts "Enter country of birth:"
@@ -86,21 +84,21 @@ def print_header
   puts "----------"
 end
 
-def search_letter(students)
+def search_letter
   puts "What letter do you want to search?"
   letter = gets.strip.upcase
   puts "Here are the students that start with '#{letter}':"
-  students.each do |student|
+  @students.each do |student|
     if (student[:name].start_with? letter)
       puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
 end
 
-def short_name(students)
+def short_name
   puts "-" * 10
   puts "These are our students with less than 12 characters in their name:"
-  students.each do |student|
+  @students.each do |student|
     length = student[:name].length
     if length <= 12
       puts "#{student[:name]} (#{student[:cohort]} cohort)"
@@ -108,19 +106,19 @@ def short_name(students)
   end
 end
 
-def print_redo(students)
-  student_count = students.count
+def print_students_list
+  student_count = @students.count
   if student_count == 0
     puts "No students were inputted."
   else
-    students.each_with_index do |student, index|
+    @students.each_with_index do |student, index|
       puts "#{index +1}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
 end
 
-def print_information(students)
-  students.each do |student|
+def print_information
+  @students.each do |student|
     puts "--------------------------".center(29)
     puts "#{student[:name]}".center(29)
     puts "Additional student information:"
@@ -130,43 +128,50 @@ def print_information(students)
   end
 end
 
-def print_footer(students)
-  count = students.count
+def print_footer
+  count = @students.count
   if count == 1
     puts "Overall, we have #{count} great student."
   elsif count == 0
     puts ''
   else count > 1
-    puts "Overall, we have #{students.count} great students."
+    puts "Overall, we have #{@students.count} great students."
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit #This causes the programme to terminate
+  else
+    puts "I don't know what you meant, try again."
   end
 end
 
 def interactive_menu
-  students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-    when "1"
-      # Input students
-      students = input_students
-    when "2"
-      # Show the students
-      print_header
-      print_redo(students)
-      print_footer(students)
-    when "9"
-      exit #This causes the programme to terminate
-    else
-      puts "I don't know what you meant, try again."
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
 interactive_menu
 print_header
-print_redo(students)
-print_information(students)
-print_footer(students)
+print_students_list
+print_footer
